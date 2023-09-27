6ebd003412e41f10020988d967bfa0d309bbcc1a
Diana Rodriguez Cosme <76630368+diannita@users.noreply.github.com>
2023-09-27 23:38:23 +1300

    file 10 - writeFileSync

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -6,11 +6,11 @@ const path = require("path");
 const filePath = path.join(__dirname, "temporary", "fileA.txt");
 
 // Write three lines to the file using writeFileSync
-fs.writeFileSync(filePath, "Line 1\n");
+fs.writeFileSync(filePath, "Line 1 - Tomorrow's weather will be amazing!..\n");
 
 // Append two more lines to the file using writeFileSync with the "append" flag
-fs.writeFileSync(filePath, "Line 2\n", { flag: "a" });
-fs.writeFileSync(filePath, "Line 3\n", { flag: "a" });
+fs.writeFileSync(filePath, "Line 2  I love coding ...\n", { flag: "a" });
+fs.writeFileSync(filePath, "Line 3 - Nodejs is pretty interesting\n", { flag: "a" });
 
 // Read the file using readFileSync and log its contents to the console
 const fileContents = fs.readFileSync(filePath, "utf-8");
