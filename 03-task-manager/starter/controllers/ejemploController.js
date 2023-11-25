80ed31b97b133dff83a7b0384227b99987074710
Diana Rodriguez Cosme <76630368+diannita@users.noreply.github.com>
2023-11-25 22:51:54 +1300

    added changes and CRUD, error handler

--- 03-task-manager/starter/controllers/ejemploController.js
+++ 03-task-manager/starter/controllers/ejemploController.js
@@ -1,41 +1,80 @@
-const Ejemplo = require('../models/ejemplo');
+const Example = require('../models/ejemplo');
+const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error');
 
-// Controlador para crear un nuevo ejemplo
-exports.createEjemplo = async (req, res) => {
-  try {
-    const ejemplo = new Ejemplo(req.body);
-    const resultado = await ejemplo.save();
-    res.json(resultado);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
+// Controller to create a new example
+exports.createExample = asyncWrapper (async (req, res) => {
+    const example = new Example(req.body);
+    const result = await example.save();
+    res.json(result);
+});
 
-// Controlador para obtener el apellido de todas las personas
-exports.getLastNames = async (req, res) => {
-  try {
-    const ejemplos = await Ejemplo.find({}, 'nombre');
-    const lastNames = ejemplos.map((e) => e.nombre.split(' ').pop());
+// Controller to get the last names of all people
+exports.getLastNames = asyncWrapper (async (req, res) => {
+    const examples = await Example.find({}, 'nombre');
+    const lastNames = examples.map((e) => e.nombre.split(' ').pop());
     res.json(lastNames);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
+});
 
-// Controlador para convertir a "Title Case"
-exports.convertToTitleCase = async (req, res) => {
-  try {
-    const ejemplos = await Ejemplo.find({}, 'nombre');
-    const titleCaseNames = ejemplos.map((e) => toTitleCase(e.nombre));
+// Controller to convert to "Title Case"
+exports.convertToTitleCase = asyncWrapper (async (req, res) => {
+    const examples = await Example.find({}, 'nombre');
+    const titleCaseNames = examples.map((e) => toTitleCase(e.nombre));
     res.json(titleCaseNames);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+});
 
-// Función para convertir a "Title Case"
+exports.updateFullName = asyncWrapper (async (req, res) => {
+    const { _id: nombreID } = req.params;
+    const { nombre } = req.body;
+
+    if (!_id || !nombre) {
+      return res.status(400).json({ message: 'Both _id and name are required in the request.' });
+    }
+
+    const updatedExample = await Example.findByIdAndUpdate(_id, { nombre: nombre }, { new: true });
+
+    if (!updatedExample) {
+      return next(createCustomError(`No name with Id: ${nombreID} was found..`, 404));
+    }
+
+    res.json(updatedExample);
+});
+
+// Controller to delete an example by ID
+exports.deleteFullName = asyncWrapper (async (req, res) => {
+    const { _id: nombreID } = req.params;
+
+    if (!_id) {
+      return res.status(400).json({ message: 'The _id parameter is required in the request.' });
+    }
+
+    const deletedExample = await Example.findByIdAndDelete(_id);
+
+    if (!deletedExample) {
+      return next(createCustomError(`No name with Id: ${nombreID} was found..`, 404));
+    }
+
+    res.json({ message: 'Example deleted successfully' });
+});
+
+// Controller to show one full name by ID
+exports.getOneFullName = asyncWrapper (async (req, res, next) => {
+    const { id: nombreID } = req.params;
+    const nombre = await Example.findOne({ _id: nombreID });
+
+    if (!nombreID) {
+      return next(createCustomError(`No name with Id: ${nombreID} was found..`, 404));
+    }
+    res.status(200).json({ nombre });
+})
+
+// Controller show all names from collections
+exports.getAllFullName = asyncWrapper (async (req, res) => {
+    const nombre = await Example.find({});
+    res.status(200).json({ nombre });
+})
+
+// Function to convert to "Title Case"
 function toTitleCase(str) {
   return str
     .toLowerCase()
--- 03-task-manager/starter/routes/ejemploRoutes.js
+++ 03-task-manager/starter/routes/ejemploRoutes.js
@@ -2,13 +2,27 @@ const express = require('express');
 const router = express.Router();
 const ejemploController = require('../controllers/ejemploController');
 
-// Ruta para crear un nuevo ejemplo
-router.post('/v1/create', ejemploController.createEjemplo);
+//CRUD => Create, Read, Update, Delete
 
-// Ruta para obtener el apellido de todas las personas
-router.get('/v1/lastnames', ejemploController.getLastNames);
+// Route to create a name
+router.post('/v1/create/name', ejemploController.createExample);
 
-// Ruta para convertir a "Title Case"
-router.get('/v1/titlecase', ejemploController.convertToTitleCase);
+// Route to get people's name and lastnames
+router.get('/v1/read/lastnames', ejemploController.getLastNames);
+
+// Route to convert "Title Case"
+router.get('/v1/read/titlecase', ejemploController.convertToTitleCase);
+
+// Route to edit people's lastnames by id
+router.put('/v1/update/name/:id', ejemploController.updateFullName);
+
+// Route to delete people's lastnames by id
+router.delete('/v1/delete/name/:id', ejemploController.deleteFullName);
+
+// Route to get one people's full name
+router.get('/v1/get-one/name/:id', ejemploController.getOneFullName);
+
+// Route to get all people's names
+router.get('/v1/get-all/names', ejemploController.getAllFullName);
 
 module.exports = router;
