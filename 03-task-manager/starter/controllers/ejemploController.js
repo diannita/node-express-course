473a27e4309aa5e4965369d986cc5d80193484d3
Diana Rodriguez Cosme <76630368+diannita@users.noreply.github.com>
2023-11-25 18:58:44 +1300

    update added rest of the CRUD

--- 03-task-manager/starter/controllers/ejemploController.js
+++ 03-task-manager/starter/controllers/ejemploController.js
@@ -1,22 +1,22 @@
-const Ejemplo = require('../models/ejemplo');
+const Example = require('../models/ejemplo');
 
-// Controlador para crear un nuevo ejemplo
-exports.createEjemplo = async (req, res) => {
+// Controller to create a new example
+exports.createExample = async (req, res) => {
   try {
-    const ejemplo = new Ejemplo(req.body);
-    const resultado = await ejemplo.save();
-    res.json(resultado);
+    const example = new Example(req.body);
+    const result = await example.save();
+    res.json(result);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
   }
 };
 
-// Controlador para obtener el apellido de todas las personas
+// Controller to get the last names of all people
 exports.getLastNames = async (req, res) => {
   try {
-    const ejemplos = await Ejemplo.find({}, 'nombre');
-    const lastNames = ejemplos.map((e) => e.nombre.split(' ').pop());
+    const examples = await Example.find({}, 'nombre');
+    const lastNames = examples.map((e) => e.nombre.split(' ').pop());
     res.json(lastNames);
   } catch (error) {
     console.log(error);
@@ -24,18 +24,63 @@ exports.getLastNames = async (req, res) => {
   }
 };
 
-// Controlador para convertir a "Title Case"
+// Controller to convert to "Title Case"
 exports.convertToTitleCase = async (req, res) => {
   try {
-    const ejemplos = await Ejemplo.find({}, 'nombre');
-    const titleCaseNames = ejemplos.map((e) => toTitleCase(e.nombre));
+    const examples = await Example.find({}, 'nombre');
+    const titleCaseNames = examples.map((e) => toTitleCase(e.nombre));
     res.json(titleCaseNames);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
-// Función para convertir a "Title Case"
+exports.updateFullName = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const { nombre } = req.body;
+
+    if (!_id || !nombre) {
+      return res.status(400).json({ message: 'Both _id and name are required in the request.' });
+    }
+
+    const updatedExample = await Example.findByIdAndUpdate(_id, { nombre: nombre }, { new: true });
+
+    if (!updatedExample) {
+      return res.status(404).json({ message: 'The User was not found.' });
+    }
+
+    res.json(updatedExample);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+// Controller to delete an example by ID
+exports.deleteFullName = async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    if (!_id) {
+      return res.status(400).json({ message: 'The _id parameter is required in the request.' });
+    }
+
+    const deletedExample = await Example.findByIdAndDelete(_id);
+
+    if (!deletedExample) {
+      return res.status(404).json({ message: 'Example not found' });
+    }
+
+    res.json({ message: 'Example deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+
+// Function to convert to "Title Case"
 function toTitleCase(str) {
   return str
     .toLowerCase()
